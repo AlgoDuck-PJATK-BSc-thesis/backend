1e672f77e2e768bcf7f06df5354e706d8332ddc9
s28128 <s28128@pjwstk.edu.pl>
2025-08-05 17:21:26 +0200

    Implementacja Contest Module: - Model Contest i ContestProblem - DTOs do tworzenia i pobierania konkursów - Serwis z metodami Create, Delete, Add/RemoveProblem - Repozytorium do obsługi Contestów - Kontroler API z endpointami do zarządzania konkursami

--- WebApplication1/Modules/ContestModule/ContestModule.cs
+++ WebApplication1/Modules/ContestModule/ContestModule.cs
@@ -1,6 +1,14 @@
+using WebApplication1.Modules.Contest.Services;
+using WebApplication1.Modules.Contest.Repositories;
 namespace WebApplication1.Modules.Contest;
 
-public class ContestModule
+public static class ContestModule
 {
-    
+    public static IServiceCollection AddContestModule(this IServiceCollection services)
+    {
+        services.AddScoped<IContestRepository, ContestRepository>();
+        services.AddScoped<IContestService, ContestService>();
+
+        return services;
+    }
 }
---
+++ WebApplication1/Modules/ContestModule/DTOs/ContestProblemDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Modules.Contest.DTOs;
+
+public class ContestProblemDto
+{
+    public Guid ContestId { get; set;}
+    public Guid ProblemId { get; set;}
+}
