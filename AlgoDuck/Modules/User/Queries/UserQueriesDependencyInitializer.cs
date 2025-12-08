dc9c2d68a0fad33ce573e17385694529025c9b50
Maja Frankowska <s24331@pjwstk.edu.pl>
2025-12-08 04:46:04 +0100

    feat: Additional Commands and Queries for Auth and User modules

--- AlgoDuck/Modules/User/Queries/UserQueriesDependencyInitializer.cs
+++ AlgoDuck/Modules/User/Queries/UserQueriesDependencyInitializer.cs
@@ -1,3 +1,7 @@
+using AlgoDuck.Modules.User.Queries.GetCohortLeaderboard;
+using AlgoDuck.Modules.User.Queries.GetLeaderboardGlobal;
+using AlgoDuck.Modules.User.Queries.GetSelectedAvatar;
+using AlgoDuck.Modules.User.Queries.GetTwoFactorEnabled;
 using AlgoDuck.Modules.User.Queries.GetUserActivity;
 using AlgoDuck.Modules.User.Queries.GetUserAchievements;
 using AlgoDuck.Modules.User.Queries.GetUserById;
@@ -5,8 +9,10 @@ using AlgoDuck.Modules.User.Queries.GetUserProfile;
 using AlgoDuck.Modules.User.Queries.GetUserSolvedProblems;
 using AlgoDuck.Modules.User.Queries.GetUserStatistics;
 using AlgoDuck.Modules.User.Queries.GetUserConfig;
+using AlgoDuck.Modules.User.Queries.GetUserLeaderboardPosition;
 using AlgoDuck.Modules.User.Queries.SearchUsers;
 using AlgoDuck.Modules.User.Queries.GetUserRankings;
+using AlgoDuck.Modules.User.Queries.GetVerifiedEmail;
 using FluentValidation;
 
 namespace AlgoDuck.Modules.User.Queries;
@@ -41,7 +47,19 @@ public static class UserQueriesDependencyInitializer
 
         services.AddScoped<IGetUserRankingsHandler, GetUserRankingsHandler>();
         services.AddScoped<GetUserRankingsValidator>();
-
+        
+        services.AddScoped<IGetUserLeaderboardPositionHandler, GetUserLeaderboardPositionHandler>();
+        
+        services.AddScoped<IGetVerifiedEmailHandler, GetVerifiedEmailHandler>();
+        
+        services.AddScoped<IGetTwoFactorEnabledHandler, GetTwoFactorEnabledHandler>();
+        
+        services.AddScoped<IGetSelectedAvatarHandler, GetSelectedAvatarHandler>();
+        
+        services.AddScoped<IGetLeaderboardGlobalHandler, GetLeaderboardGlobalHandler>();
+        
+        services.AddScoped<IGetCohortLeaderboardHandler, GetCohortLeaderboardHandler>();
+        
         return services;
     }
 }
